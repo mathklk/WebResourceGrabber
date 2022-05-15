6d88a7ccd6882b0055ced49c034e4e8175121f4c
Mathis Kölker <mathis@klker.de>
2022-05-15 12:37:56 +0200

    command line interface

--- README.md
+++ README.md
@@ -2,11 +2,36 @@
 
 A crude python script to grab resources (fonts, images) from web pages.
 
-
 For a given URL, this script scans the html for any referenced css files.
 Any fonts and images that the css file mentions, are downloaded and placed in a folder named `out`.
+
+## Usage
+
+```shell script
+./main.py https://www.example.com
+```
+
+You should verify that the exported files are correctly formatted.
+The command line utility `file` is perfect for this.
+cd into the generated subdirectory and type `file *`.
+If it says "empty", "ascii text" or "html document" for any of the files, then there was likely an error for that particular file.
 
 ## Fonts
 
 Fonts are often times in the [WOFF](https://en.wikipedia.org/wiki/Web_Open_Font_Format) format.
 There a numerous online converters to convert them to otf or ttf, or use [this](https://github.com/hanikesn/woff2otf) tool.
+
+## Images
+
+Only works for PNG images at the moment.
+
+## Does it actually work?
+
+Meh.
+
+It depends largely on which web page you scan.
+This program does not have all the functionality that a web browser has (it does not have javascript).
+On modern web pages it will probably miss a lot of resources.
+
+This program is unbelievably sketchy and botched together. 
+It's safe to say you should not use it for serious purposes or even look at the source code.
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+#!/usr/bin/python3
+import argparse
 import requests
 from typing import *
 from urllib.parse import urlparse
 import re
 import os
-
-tprint = lambda x: print(f"[{type(x)}] {x}")
+import base64
+import hashlib
 
 
 def getCssPaths(url: str) -> List[str]:
@@ -32,12 +34,12 @@ def getCssPaths(url: str) -> List[str]:
     return css_paths
 
 
-def getBulkPaths(url: str, paths: List[str]) -> List[str]:
+def getBulkPaths(url: str, paths: Iterable[str]) -> List[str]:
     """
-    Tries to get the contents of multiple paths ofa respective url
+    Tries to get the contents of multiple paths of a respective url
 
     :param url: ROOT-URL WITHOUT FOLLOWING SLASH
-    :param cssPaths: any amount of sub-paths
+    :param paths: any amount of sub-paths
     :return:
     """
 
@@ -52,10 +54,10 @@ def getBulkPaths(url: str, paths: List[str]) -> List[str]:
                 could_be_url = url_root + path
             r = requests.get(could_be_url)
             if r.status_code == 200:
-                print(could_be_url, 'returned with status code <200> - seems to be a legit file')
+                print('\t', could_be_url, 'returned with status code <200> - seems to be a legit file')
                 content_list.append(r.text)
             else:
-                print(could_be_url, 'returned with status code', r.status_code)
+                print('\t', could_be_url, 'returned with status code', r.status_code)
 
         except ...:
             pass
@@ -69,41 +71,70 @@ def getUrlsFromCss(css: str) -> List[str]:
     return [u[4:-1].strip('\"\'') for u in raws]
 
 
-url = "https://www.iana.org/domains/reserved"
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", nargs=1)
+    args = parser.parse_args()
+    url = args.url[0]
+    url_components = urlparse(url)
+    assert url_components.scheme
+    assert url_components.hostname
+    url_root: str = url_components.scheme + '://' + url_components.hostname
+    if url_components.port is not None:
+        url_root += ':' + str(url_components.port)
 
-url_components = urlparse(url)
-url_root: str = url_components.scheme + '://' + url_components.hostname
-if url_components.port is not None:
-    url_root += ':' + str(url_components.port)
+    css_paths = getCssPaths(url)
+    css_paths = set(css_paths)  # filter duplicates
+    css_contents = getBulkPaths(url_root, css_paths)
+    css_mess = ''.join(css_contents)
+    urls_or_paths = getUrlsFromCss(css_mess)
 
-css_paths = getCssPaths(url)
-css_contents = getBulkPaths(url_root, css_paths)
-css_mess = ''.join(css_contents)
-urls_or_paths = getUrlsFromCss(css_mess)
-absolute_res_urls = []
-for url_or_path in urls_or_paths:
-    isData = url_or_path.startswith('data:')
-    if isData:
-        continue
-    elif 'http' in url_or_path:
-        absolute_res_urls.append(url_or_path)
-    else:
-        absolute_res_urls.append(url_root + url_or_path)
+    b64_datas = []
+    absolute_res_urls = []
+    for url_or_path in urls_or_paths:
+        isData = url_or_path.startswith('data:')
+        if isData:  # Content is b64 (probably) data
+            b64_datas.append(url_or_path)
+        elif 'http' in url_or_path:  # Resource is an absolute path
+            absolute_res_urls.append(url_or_path)
+        else:  # Resource is a relative path
+            absolute_res_urls.append(url_root + url_or_path)
 
+    if not os.path.exists('./out'):
+        os.mkdir('./out')
 
-
-if not os.path.exists('./out'):
-    os.mkdir('./out')
-
-for url in absolute_res_urls:
-    assert 'http' in url
     foldername = url_components.hostname + url_components.path.replace('/', '_')
-    if not os.path.exists('./out/' + foldername):
-        os.mkdir('./out/' + foldername)
+    subdir = './out/' + foldername
+    if not os.path.exists(subdir):
+        os.mkdir(subdir)
 
-    filename = url.split('/')[-1]
-    print('Getting', filename, '...')
-    r = requests.get(url)
-    data = r.content
-    with open('./out/' + foldername + '/' + filename, 'wb+') as f:
-        f.write(data)
+    print('Processing web resources..')
+    if not absolute_res_urls:
+        print('\tNo resources referenced via urls, skipping.')
+    for url in absolute_res_urls:
+        assert url.startswith('http')
+        filename = url.split('/')[-1]
+        print('Getting', filename, '..')
+        r = requests.get(url)
+        data = r.content
+        dest = subdir + '/' + filename
+        with open(dest, 'wb+') as f:
+            f.write(data)
+        print('\tExported', dest)
+
+    print('Processing data resources..')
+    if not b64_datas:
+        print('\tNo embedded b64 data, skipping.')
+    for d in b64_datas:
+        if 'base64' not in d or 'image/png' not in d:
+            continue
+        filetype = d.split(';')[0].split('/')[-1]  # syntax assumed from image/png file
+        b64str = d.split(',')[-1]
+        print('\td  :: ' + d)
+        print('\tb64:: ' + b64str)
+        bytedata = base64.b64decode(b64str)
+        filename = hashlib.sha1(bytedata).hexdigest()  # use sha sum of content as filename because why not
+        file = subdir + '/' + filename + '.' + filetype
+        with open(file, 'wb+') as f:
+            f.write(bytedata)
+        print('\tExported', file)
